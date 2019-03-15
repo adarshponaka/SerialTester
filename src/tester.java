be85efd32829cd2a448e8ea9c231d9397e44579a
Adarsh Ponaka <adarshponaka@gmail.com>
2019-03-15 19:09:26 -0400

    updated serial code (sending and receibing)

--- src/tester.java
+++ src/tester.java
@@ -1,11 +1,29 @@
-import arduino.*;
+import jssc.SerialPort;
+import jssc.SerialPortException;
 
 public class tester {
-    public static void main (String[] args){
-        Arduino obj = new Arduino();
-        obj.openConnection();
-        System.out.println(obj.getPortDescription());
 
+    public static void main(String[] args) {
+        SerialPort serialPort = new SerialPort("COM6");
+
+        try {
+            serialPort.openPort();
+
+            serialPort.setParams(SerialPort.BAUDRATE_9600,
+                    SerialPort.DATABITS_8,
+                    SerialPort.STOPBITS_1,
+                    SerialPort.PARITY_NONE,
+                    true, false);
+
+            serialPort.setFlowControlMode(SerialPort.FLOWCONTROL_RTSCTS_IN
+                    | SerialPort.FLOWCONTROL_RTSCTS_OUT);
+
+            System.out.println("Angle: " + serialPort.readString());
+            System.out.println("String sent: " + serialPort.writeString("20"));
+
+            serialPort.closePort();
+        } catch (SerialPortException ex) {
+            System.out.println("There is an error on writing string to port: " + ex);
+        }
     }
-
 }
